329fa7ac972130414a233fdf2727e44054259c1f
lipa88-mk <lipa88@mail.ru>
2021-09-15 12:59:45 +0300

    добавлен маркер центра Токио

--- js/leaflet-map.js
+++ js/leaflet-map.js
@@ -16,3 +16,30 @@ L.tileLayer(
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   },
 ).addTo(map);
+
+
+// marker
+
+const mainPinIcon = L.icon({
+  iconUrl: '../img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const marker = L.marker(
+  {
+    lat: 35.67963,
+    lng: 139.77379,
+  },
+  {
+    //draggable: true,
+    icon: mainPinIcon,
+  },
+);
+marker.addTo(map);
+
+// mainPinMarker.on('moveend', (evt) => {
+//   console.log(evt.target.getLatLng());
+// });
+
+// mainPinMarker.remove();

0665c248afe79305d348b14d5635b26e95914c33
lipa88-mk <lipa88@mail.ru>
2021-09-15 13:17:44 +0300

    перемещение и получение координат маркера

--- js/leaflet-map.js
+++ js/leaflet-map.js
@@ -8,7 +8,7 @@ const map = L.map('map-canvas')
   .setView({
     lat: 35.67963,
     lng: 139.77379,
-  }, 10);
+  }, 12);
 
 L.tileLayer(
   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -26,20 +26,23 @@ const mainPinIcon = L.icon({
   iconAnchor: [26, 52],
 });
 
-const marker = L.marker(
+const mainPinMarker = L.marker(
   {
     lat: 35.67963,
     lng: 139.77379,
   },
   {
-    //draggable: true,
+    draggable: true,
     icon: mainPinIcon,
   },
 );
-marker.addTo(map);
+mainPinMarker.addTo(map);
 
-// mainPinMarker.on('moveend', (evt) => {
-//   console.log(evt.target.getLatLng());
-// });
+mainPinMarker.on('moveend', (evt) => {
+  // console.log(evt.target.getLatLng());
+  const x = evt.target.getLatLng().lat.toFixed(5);
+  const y = evt.target.getLatLng().lng.toFixed(5);
+  console.log(x, y);
+});
 
 // mainPinMarker.remove();

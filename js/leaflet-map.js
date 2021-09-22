97f953d6e7553c2d14f753c3253c765e4e7141f1
lipa88-mk <lipa88@mail.ru>
2021-09-22 16:17:35 +0300

    показ балуна с информацией

--- js/leaflet-map.js
+++ js/leaflet-map.js
@@ -19,12 +19,13 @@ L.tileLayer(
 
 
 // marker
-
+//  главный перемещаемый маркер
 const mainPinIcon = L.icon({
   iconUrl: '../img/main-pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52],
 });
+
 
 const mainPinMarker = L.marker(
   {
@@ -47,21 +48,101 @@ mainPinMarker.on('moveend', (evt) => {
 // mainPinMarker.remove();
 
 
-// метки mock-объявлений
-// метка
+// маркеры объявлений
 const cardPinIcon = L.icon({
   iconUrl: '../img/pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52],
 });
-
+// забрали объявления
 const cards = AUTHORS;
-console.log(cards);
 
+// наполнение попапа card данными объявления mockAuthor
+const createCustomPopup = (mockAuthor) => {
+  const card = document.querySelector('#card').content.querySelector('.popup').cloneNode(true);
+
+  const avatar = card.querySelector('.popup__avatar');
+  if (mockAuthor.avatar) {avatar.setAttribute('src', mockAuthor.avatar);}
+  else {avatar.classList.add('hidden');}
+
+  const title = card.querySelector('.popup__title');
+  if (mockAuthor.offer.title) {title.textContent = mockAuthor.offer.title;}
+  else {title.classList.add('hidden');}
+
+  const address = card.querySelector('.popup__text--address');
+  if (mockAuthor.offer.address) {address.textContent = mockAuthor.offer.address;}
+  else {address.classList.add('hidden');}
+
+  const price = card.querySelector('.popup__text--price');
+  if (mockAuthor.offer.price) {price.textContent = `${mockAuthor.offer.price} ₽/ночь`;}
+  else {price.classList.add('hidden');}
+
+  const capacity = card.querySelector('.popup__text--capacity');
+  if (mockAuthor.offer.rooms || mockAuthor.offer.guests) {capacity.textContent = `${mockAuthor.offer.rooms} комнаты для ${mockAuthor.offer.guests} гостей`;}
+  else {capacity.classList.add('hidden');}
+
+  const time = card.querySelector('.popup__text--time');
+  if (mockAuthor.offer.checkin || mockAuthor.offer.checkout) {time.textContent = `${mockAuthor.offer.checkin}, выезд до ${mockAuthor.offer.checkout}`;}
+  else {time.classList.add('hidden');}
+
+  const description = card.querySelector('.popup__description');
+  if (mockAuthor.offer.description) {description.textContent = mockAuthor.offer.description;}
+  else {description.classList.add('hidden');}
+
+  const type = card.querySelector('.popup__type');
+  if (mockAuthor.offer.type) {
+    switch (mockAuthor.offer.type) {
+      case 'flat':
+        type.textContent = 'Квартира';
+        break;
+      case 'bungalow':
+        type.textContent = 'Бунгало ';
+        break;
+      case 'house':
+        type.textContent = 'Дом';
+        break;
+      case 'palace':
+        type.textContent = 'Дворец';
+        break;
+      default:
+        type.textContent = 'Квартира';
+    }
+  } else {type.classList.add('hidden');}
+
+  const features = card.querySelector('.popup__features');
+  const featuresList = mockAuthor.offer.features;
+  if (featuresList) {
+    for (let featureItem of featuresList)  {
+      const listItem = document.createElement('li');
+      listItem.classList.add('popup__feature');
+      listItem.classList.add(`popup__feature--${featureItem}`);
+      features.append(listItem);
+    }
+  } else {features.classList.add('hidden');}
+
+
+  const photos = card.querySelector('.popup__photos');
+  const photo = card.querySelector('.popup__photo').cloneNode(true);
+  photos.removeChild(photos.children[0]);
+
+  const photosList = mockAuthor.offer.photos;
+  if (photosList) {
+    for (let photoItem of photosList)  {
+      const newPhoto = photo.cloneNode(true);
+      newPhoto.setAttribute('src', photoItem);
+      photos.append(newPhoto);
+    }
+  } else {photos.classList.add('hidden');}
+  return card;
+};
+
+
+// для каждого объявления создали метку
 cards.forEach((card) => {
   const lat = card.location.x;
   const lng = card.location.y;
 
+  // сощдали амркер объявления
   const marker = L.marker({
     lat,
     lng,
@@ -70,5 +151,11 @@ cards.forEach((card) => {
     icon: cardPinIcon,
   });
 
-  marker.addTo(map);
+  // добавили маркер на карту, добавили при клике вызов генерации попапа
+  marker.addTo(map).bindPopup(
+    createCustomPopup(card),
+    {
+      keepInView: true,
+    },
+  );
 });
